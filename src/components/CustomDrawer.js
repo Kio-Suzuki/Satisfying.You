b29df55a16bdca487099063c104acf35750aae61
Akio Suzuki <kiosuzuki1@gmail.com>
2024-04-22 09:50:51 -0300

    Update CustomDrawer.js
    -Added padding

--- src/components/CustomDrawer.js
+++ src/components/CustomDrawer.js
@@ -3,27 +3,25 @@ import { View, Text, StyleSheet } from 'react-native'
 import Icon from 'react-native-vector-icons/MaterialIcons'
 
 const CustomDrawer = (props) => {
-  return (
-    <DrawerContentScrollView {...props} contentContainerStyle={{ flex: 1 }}>
+    return (
+        <DrawerContentScrollView {...props} style={{padding: 20}}>
 
-      <View>
-        <Text style={{fontSize: 28, color: '#FFFFFF', fontFamily: 'AveriaLibre-Regular', alignSelf: 'center'}}>usuario@dominio.com</Text>
-        <View style={{height: 2, backgroundColor: '#FFFFFF', marginVertical: 25}} />
-      </View>
+            <View>
+                <Text style={{fontSize: 28, color: '#FFFFFF', fontFamily: 'AveriaLibre-Regular', alignSelf: 'center'}}>usuario@dominio.com</Text>
+                <View style={{height: 2, backgroundColor: '#FFFFFF', marginVertical: 25}} />
+            </View>
 
-      <DrawerItemList {...props} />
+                <DrawerItemList {...props} />
 
-      <View style={{justifyContent: 'flex-end', flex: 1}}>
-        <DrawerItem
-          labelStyle={{ fontSize: 28, color: '#FFFFFF', fontFamily: 'AveriaLibre-Regular' }}
-          label="Sair"
-          onPress={() => props.navigation.popToTop()}
-          icon={() => <Icon name="logout" size={40} color="#FFFFFF" />}
-        />
-      </View>
-
-    </DrawerContentScrollView>
-  )
+            <View style={{ paddingTop: 340 }}>
+                <DrawerItem 
+                labelStyle={{fontSize: 28, color: '#FFFFFF', fontFamily: 'AveriaLibre-Regular'}} 
+                label='Sair' onPress={() => {props.navigation.popToTop()}} 
+                icon={() => <Icon name="logout" size={40} color="#FFFFFF"/>}/>
+            </View>
+        
+        </DrawerContentScrollView>
+    )
 }
 
 export default CustomDrawer
